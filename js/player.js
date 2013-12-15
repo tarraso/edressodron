9963c93c798db68278960bd57ec7efd77d7d1b43
Taras Volchenko <exeq00@gmail.com>
2013-12-15 16:01:02 +0700

    Simple collision detection

--- js/player.js
+++ js/player.js
@@ -11,7 +11,6 @@
     var QUAD_SPEED_FACTOR = 1e-3;
     var CAR_ACCELERATION = 0.5;
 
-
     var Player = function(){
         this.initialize();
     }
@@ -19,7 +18,7 @@
     var p = Player.prototype = new createjs.Container();
     p.Container_initialize = p.initialize;
 
-    p.initialize = function(){
+    Player.prototype.initialize = function(){
         p.Container_initialize();
         var shape = new createjs.Shape();
         shape.graphics.beginFill("red").drawRect(-25,-50,50,100).endFill();
@@ -28,7 +27,7 @@
         this.on("tick", this.update);
     }
 
-    p.reset = function(){
+    Player.prototype.reset = function(){
         this.width = 100;
         this.height = 200;
         this.x = 512;
@@ -39,7 +38,7 @@
         this.rotation_speed = 0;
     };
 
-    p.update = function(){
+    Player.prototype.update = function(){
         this.x += this.velocity_x;
         this.y += this.velocity_y;
         if(KeyReader.isKeyPressed(KEY_LEFT)){
@@ -67,7 +66,31 @@
         var a_y = car_v_y * CAR_ACCELERATION - this.velocity_y*quad_speed*QUAD_SPEED_FACTOR + fr_y*PERPENDICULAR_FRICTION_FACTOR;
         this.velocity_x += a_x;
         this.velocity_y += a_y;
-        console.log(fr_x>0,fr_y>0);
+        this.detect_collisoins();
     }
+
+    Player.prototype.detect_collisoins = function(){
+        var tiles = level.children;
+        var computed_sin = Math.sin(this.rotation/180*Math.PI);
+        var computed_cos = Math.cos(this.rotation/180*Math.PI)
+        var points = [
+            [this.x + 50*computed_sin + 25*computed_cos, this.y + 50*computed_cos + 25*computed_sin ],
+            [this.x - 50*computed_sin - 25*computed_cos, this.y + 50*computed_cos + 25*computed_sin ],
+            [this.x + 50*computed_sin + 25*computed_cos, this.y - 50*computed_cos - 25*computed_sin ],
+            [this.x - 50*computed_sin - 25*computed_cos, this.y - 50*computed_cos - 25*computed_sin ],
+        ];
+        var l = tiles.length
+        for(var i=0; i<l ;i++){
+            for(var j=0; j<4; j++){
+                var tile = tiles[i];
+                var x = points[j][0];
+                var y = points[j][1]
+                if((x>tile.x)&&(x<(tile.x+tile.width))&&(y>tile.y)&&(y<tile.y+tile.height)){
+                    
+                }
+            }
+        }
+    }
+
     window.Player = Player;
 })();
